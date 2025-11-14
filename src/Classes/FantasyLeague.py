fc6b09524eedd071ad0d3d4c2a89124dccbc0d8c
arthur <arthurdeolima@gmail.com>
2025-11-14 21:18:06 +0100

    Automatic Seeding

--- src/Classes/FantasyLeague.py
+++ src/Classes/FantasyLeague.py
@@ -5,24 +5,29 @@ import requests as rq
 import json
 
 from Team import Team, WeekPerformance
+from SeedCalculator import SeedCalculator
 
 class FantasyLeague:
-    def __init__(self, from_json=None, league_id=None, seeding=None, divisions=None):
+    def __init__(self, from_json=None, league_id=None, divisions=None):
         if from_json:
             # Load configuration from JSON file
             with open(from_json, 'r', encoding='utf-8') as f:
                 config = json.load(f)
             self.league_id = config.get('league_id')
-            seeding = config.get('seeding')
             divisions = config.get('divisions')
         else:
             self.league_id = league_id
 
         self.teams = {}
-        self.retrieve_teams(seeding, divisions)
+        self.seeding_calculator = SeedCalculator()
+        self.retrieve_teams(divisions)
         self.retrieve_scoring()
+        self.update_seeding()
 
-    def retrieve_teams(self, seeding, divisions):
+    def update_seeding(self):
+        self.seeding_calculator.calculate_and_update_seeding(self.teams)
+
+    def retrieve_teams(self, divisions):
         # Fetch users data
         response = rq.get('https://api.sleeper.app/v1/league/{}/users'.format(self.league_id))
         users_data = json.loads(response.text)
@@ -36,12 +41,7 @@ class FantasyLeague:
         for roster in rosters_data:
             owner_to_roster[roster['owner_id']] = roster['roster_id']
 
-        # Create mappings from seeding and divisions config
-        seed_map = {}
-        if seeding:
-            for seed_entry in seeding:
-                seed_map[seed_entry['short_name']] = seed_entry['seed']
-
+        # TODO: RETRIEVE DIVISIONS FROM API
         division_by_team = {}
         if divisions:
             for division in divisions:
@@ -59,11 +59,9 @@ class FantasyLeague:
                 # Extract short_name (second word of team name)
                 short_name = team_name.split()[1] if len(team_name.split()) > 1 else team_name
 
-                # Get seed and division from config
-                seed = seed_map.get(short_name)
                 division = division_by_team.get(short_name)
 
-                team = Team(team_name, roster_id, division, seed)
+                team = Team(team_name, roster_id, division)
                 teams[roster_id] = team
 
         self.teams = teams
@@ -135,10 +133,12 @@ class FantasyLeague:
                 # Get opponent data
                 adversary_points = opponent["points"] if opponent else None
                 adversary_rank = opponent["rank"] if opponent else None
+                adversary_id = opponent["roster_id"] if opponent else None
+                divisional_game = self.teams[roster_id].division == self.teams[adversary_id].division if adversary_id else False
 
                 team = self.teams.get(roster_id)
                 if team:
-                    week_perf = WeekPerformance(week, points, rank, adversary_points, adversary_rank)
+                    week_perf = WeekPerformance(week, points, rank, divisional_game, adversary_id, adversary_points, adversary_rank)
                     team.insert_week(week_perf)
 
     def getTeamsData(self):
